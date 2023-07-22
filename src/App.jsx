693aeabee41743e0d7dbbc36dbe1371fd942bf9b
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-07-22 22:05:39 +0600

    added PostsView Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import CounterView from "./features/counter/CounterView";
+import PostsView from "./features/posts/PostsView";
 
 const App = () => {
   return (
     <div>
-      <h1>Counter App</h1>
+      <h1>React Toolkit App</h1>
       <CounterView></CounterView>
+      <PostsView></PostsView>
     </div>
   );
 };
--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
-  const res = await axios.get("https://jsonplaceholder.typicode.com/postss");
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
   return res.data;
 });
 
