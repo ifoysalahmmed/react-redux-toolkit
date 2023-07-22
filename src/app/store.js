6a3589238271f19c11f57323b5a0fe3dfd6e446b
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-07-22 21:45:56 +0600

    added postReducer to store file

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import postReducer from "../features/posts/postSlice";
 
 const store = configureStore({
   reducer: {
     // renaming the counterSlice to counterReducer
     counter: counterReducer,
+    // renaming the postSlice to postReducer
+    posts: postReducer,
   },
 });
 
